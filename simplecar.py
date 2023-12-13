9ff8d1e6066f7f6c3484c2d2436b62be4be41345
ashleymuka <153102801+ashleymuka@users.noreply.github.com>
2023-12-12 21:04:01 -0600

    Update simplecar.py

--- simplecar.py
+++ simplecar.py
@@ -1,18 +1,13 @@
 '''
-Author: Ashley Muka
-Assignment Title: Simple Car
-Assignment Description: Given two integers that represent the miles to drive forward and the miles to drive in reverse as user inputs, create a SimpleCar object that performs the following operations:
-Drives input number of miles forward
-Drives input number of miles in reverse
-Honks the horn
-Reports car status
-Due Date:08/23/2023
-Date Created:08/23/2023
-Date Last Modified:08/23/2023
+Assignment Description: Given two integers that represent the miles to 
+drive forward and the miles to drive in reverse as user inputs, 
+create a SimpleCar object that performs the following operations:
+1. Drives input number of miles forward
+2. Drives input number of miles in reverse
+3. Honks the horn
+4. Reports car status
 
 '''
-
-
  
 class SimpleCar:
     def __init__(self):
@@ -30,19 +25,12 @@ class SimpleCar:
     def status(self):
         print(f'Car has driven: {self.miles} miles')
 
-
 if __name__ == "__main__":
-
-#data abstraction
-    
+  
     car = SimpleCar()
-
-#input
     
     forward = int(input())
     backup = int(input())
-
-#process and output
 
     car.drive_forward(forward)
     car.drive_reverse(backup)
